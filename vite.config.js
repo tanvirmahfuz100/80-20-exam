f7871a3d94a268f632a3ad18f30fe156acedb608
Faisal <famin22.ais@bu.ac.bd>
2026-05-21 17:16:06 +0600

    opening issue

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/80-20-exam/', // Required for GitHub Pages deployment
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
