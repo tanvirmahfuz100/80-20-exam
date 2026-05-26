28d4b06b8313fbb9762fd6dce96613c4e87f4700
Md. Mahfuzur Rahman Tanvir <tanvirmahfuz100@gmail.com>
2026-05-26 13:15:55 +0600

    Update vite.config.js
    Deploying in Vercel

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/80-20-exam/',
+  base: process.env.VITE_BASE_PATH || '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
